c2924417cf2f76f10ce30a7af2dbc00dd3d5717c
pszmajduch <pszmajduch@evertop.pl>
2020-08-27 23:40:16 +0200

    add swagger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,9 @@
 delete from Event_Details;
 delete from USERS_CREDENTIALS;
 insert into Event_Details(event_Id,title,info,type_event,location,date_Of,img_Ext)
-values (1,'Miedzy nami trupami','test info','Spektakl','test location',TO_DATE('17/12/2015', 'DD/MM/YYYY'),'jpg');
+values (1,'Miedzy nami trupami','test info','Spektakl','test location',TO_DATE('17/08/2020', 'DD/MM/YYYY'),'jpg');
 insert into Event_Details(event_Id,title,info,type_event,location,date_Of,img_Ext)
-values (2,'Dobrze się kłamie','test info','Spektakl','test location',TO_DATE('17/12/2015', 'DD/MM/YYYY'),'jpg');
+values (2,'Dobrze się kłamie','test info','Spektakl','test location',TO_DATE('17/12/2020', 'DD/MM/YYYY'),'jpg');
 insert into Event_Details(event_Id,title,info,type_event,location,date_Of,img_Ext)
 values (3,'Pinokio','test info2','Spektakl','test location2',TO_DATE('17/12/2017', 'DD/MM/YYYY'),'jpg');
 
